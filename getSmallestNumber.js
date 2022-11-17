d482e95fa655e29e658d1d19cfd0435503f09304
Marcin Laszczynski <mar.zczsal@gmail.com>
2022-11-17 19:32:58 +0100

    added getSmallestNumber function

--- getSmallestNumber.js
+++ getSmallestNumber.js
@@ -1,6 +1,14 @@
 // Write the getSmallestNumber function using the forEach function
 
+function getSmallestNumber(arrayOfNumbers) {
+    let smallestNumber = arrayOfNumbers[0];
+    arrayOfNumbers.forEach(function(number) {
+        if (smallestNumber > number) {
+            smallestNumber = number;
+        }
+    });
+    return smallestNumber;
+}
 
-
-getSmallestNumber([2, -5, 10, 1, 4]); // -5
-getSmallestNumber([200, 25, 4, 123, 87]); // 4
+console.log(getSmallestNumber([2, -5, 10, 1, 4])); // -5
+console.log(getSmallestNumber([200, 25, 4, 123, 87])); // 4

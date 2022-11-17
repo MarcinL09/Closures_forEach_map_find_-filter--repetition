4ce2a33b578295da1a9f36269cc9eb50be2c943e
Marcin Laszczynski <mar.zczsal@gmail.com>
2022-11-17 20:54:27 +0100

    added getDivideByFunction function

---
+++ getDivideByFunction.js
@@ -0,0 +1,14 @@
+// write the getDivideByFunction
+
+function getDivideByFunction(firstNumber) {
+    return function(secondNumber) {
+        return secondNumber / firstNumber;
+    }
+}
+
+const divideByFive = getDivideByFunction(5);
+console.log(divideByFive(10)); // 2
+console.log(divideByFive(50)); // 10
+const divideByTwo = getDivideByFunction(2);
+console.log(divideByTwo(8)); // 4
+console.log(divideByTwo(50)); // 25

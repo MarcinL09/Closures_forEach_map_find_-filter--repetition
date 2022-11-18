739bc2c1ec6bec9700a3dbd312f752ecde1912a5
Marcin Laszczynski <mar.zczsal@gmail.com>
2022-11-18 19:20:30 +0100

    added 'countSheep' function

---
+++ countSheep.js
@@ -0,0 +1,14 @@
+function countSheep(arrayOfSheep) {
+    const sheepCounter = arrayOfSheep.filter(function (sheep){
+       return sheep === true;
+    });
+    return sheepCounter.length;
+}
+
+
+console.log(countSheep( [true,  true,  true,  false,
+                                    true,  true,  true,  true ,
+                                    true,  false, true,  false,
+                                    true,  false, false, true ,
+                                    true,  true,  true,  true ,
+                                    false, false, true,  true]));

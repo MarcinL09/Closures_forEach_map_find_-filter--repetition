dc9306b6c4a86cf60d8ddf35bb57295f0d494e6e
Marcin Laszczynski <mar.zczsal@gmail.com>
2022-11-19 21:33:46 +0100

    little improve of 'findNeedle' function

--- findNeedle.js
+++ findNeedle.js
@@ -1,8 +1,8 @@
 function findNeedle(haystack) {
-    const find = haystack.findIndex(function(element){
+    const find = haystack.findIndex(function(element) {
        return element === "needle";
     });
     return `found the needle at position ${find}`;
 }
 
-console.log(findNeedle(["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"]));// "found the needle at position 5"
+console.log(findNeedle(["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"])); // "found the needle at position 5"
